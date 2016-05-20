cbb10d1454d5c0163a6cee5a358a7afeeb6e787d
Noah Huppert <noahuppert@gmail.com>
2016-05-20 03:05:00 -0400

    renamed authentication => auth, +auth Makefile

--- app/auth/main.go
+++ app/auth/main.go
@@ -2,12 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func root(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"key": "value",
+	})
+}
+
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Ok")
-	})
+	router.GET("/", root) 
 
 	router.Run(":3000")
 }
